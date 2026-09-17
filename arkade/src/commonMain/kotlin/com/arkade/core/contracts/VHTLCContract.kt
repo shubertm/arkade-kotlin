1669a074fb9079329228f57fdcd42540624284ce
Shubert Munthali <shubertm.m@gmail.com>
2026-09-17 17:39:23 +0200

    Check locktime against blockchain median past time

--- arkade/src/commonMain/kotlin/com/arkade/core/contracts/VHTLCContract.kt
+++ arkade/src/commonMain/kotlin/com/arkade/core/contracts/VHTLCContract.kt
@@ -127,12 +127,13 @@ class VHTLCContract(
             )
         }
 
-        val chainTime = requireNotNull(chainTimeProvider) { "Missing chain time provider" }.getChainTime()
+        val chainTimeProvider = requireNotNull(chainTimeProvider) { "Missing chain time provider" }
+
         val refundElapsed =
             if (refundLockTime.isTimeLock()) {
-                refundLockTime < chainTime.time
+                refundLockTime < chainTimeProvider.getMedianTimePast()
             } else {
-                chainTime.height >= refundLockTime
+                chainTimeProvider.getChainTime().height >= refundLockTime
             }
 
         if (refundElapsed) {
